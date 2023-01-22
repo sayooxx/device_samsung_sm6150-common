a7584cd4eebdf44a387d0b126a4be8194ce941d5
LuK1337 <priv.luk@gmail.com>
2023-01-22 21:51:31 +0100

    sm6150-common: Build libhidltransport/libhwbinder from source
    * Needed for old blobs.
    
    Change-Id: I7885573e6aeb2f9edf4928b529cf6f2e278548ce

--- sm6150.mk
+++ sm6150.mk
@@ -300,6 +300,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service \
 
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor \
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung \
