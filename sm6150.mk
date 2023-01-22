1368c0a621eef7e676950da3e8f3b0566f4e50df
Patrick Tjin <pattjin@google.com>
2023-01-22 12:11:14 +0100

    sm6150-common: Add no SDCard to AAPT characteristics
    Change-Id: Ie943408246e5dfcd753ad72fbc9b525c04768a52

--- sm6150.mk
+++ sm6150.mk
@@ -25,6 +25,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 COMMON_PATH := device/samsung/sm6150-common
 
 PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_TARGET_VNDK_VERSION := 30
 PRODUCT_USE_DYNAMIC_PARTITIONS := false
 
