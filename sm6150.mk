491ea7ededa32d0b8a83567cf7ca06b3a0fab5ff
Jaekyun Seok <jaekyun@google.com>
2023-01-22 18:46:10 +0100

    sm6150-common: Enforce RROs for all build-time overlays
    Change-Id: If07ecad7b12b348203318705544d1f948745c70f

--- sm6150.mk
+++ sm6150.mk
@@ -240,6 +240,9 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/libnfc-qrd-SN100.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-qrd-SN100.conf \
     $(COMMON_PATH)/rootdir/vendor/etc/libnfc-qrd-SN100_38_4MHZ.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-qrd-SN100_38_4MHZ.conf \
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Performance
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
