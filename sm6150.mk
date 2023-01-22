2c3385f6d3ec26c556dcd11a1aa0c0a522a901fc
shoudil <stephenli@codeaurora.org>
2023-01-22 22:23:14 +0100

    sm6150-common: Declare that we support full value addons
    Change-Id: I9c571c828a19398234e6d42b38d635c941b8f67c

--- sm6150.mk
+++ sm6150.mk
@@ -503,6 +503,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
 
+# QMI
+TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
+
 # QTI
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/etc/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
