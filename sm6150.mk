0223aab51f48af31c6c4c35370e5307fb276cebe
dianlujitao <dianlujitao@lineageos.org>
2023-01-22 22:09:16 +0100

    sm6150-common: Build the QTI perf libs from source
    Change-Id: I69eec855d9661ebcc70161a44605a190989aa973

--- sm6150.mk
+++ sm6150.mk
@@ -445,6 +445,14 @@ PRODUCT_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay-lineage \
 
 # Performance
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0 \
+    vendor.qti.hardware.perf@2.1 \
+    vendor.qti.hardware.perf@2.2 \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor \
+
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
     $(COMMON_PATH)/rootdir/vendor/etc/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
