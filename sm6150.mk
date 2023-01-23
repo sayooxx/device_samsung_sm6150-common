f006865c15dc248e051b1a04c1d8026113cdfd25
Lucchetto <lucchetto.tie@live.com>
2023-01-23 15:05:59 +0100

    sm6150-common: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking optical.
    * we could even map the functions to use libperfmgr powerhints in the future.
    
    [SebaUbuntu]: Cleanup Android.bp and add copyright header.
    
    Change-Id: I777b4c615091b72d815b89ffbe058462fc460918

--- sm6150.mk
+++ sm6150.mk
@@ -448,6 +448,10 @@ PRODUCT_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
     $(COMMON_PATH)/overlay-lineage \
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client \
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
