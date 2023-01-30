71dcdf7faa3aaa4fabc6dbabb5932aba0a2d466b
Aaron Kling <webgeek1234@gmail.com>
2023-01-30 22:41:54 +0100

    sm6150-common: Remove references to a2dp module
    APM would fail to parse the config anyway:
    
      E DevicesFactoryHAL: loadAudioInterface couldn't load audio hw module audio.a2dp (No such file or directory)
      W DevicesFactoryHalHidl: The specified device name is not recognized: "a2dp"
      E AudioFlinger: loadHwModule() error -22 loading module a2dp
      W APM_AudioPolicyManager: could not open HW module a2dp
    
    Change-Id: I5ab8e0a7a8fce0e1702c81ca0537987ef2e3ebcd

--- sm6150.mk
+++ sm6150.mk
@@ -54,7 +54,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl \
 
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
     audio.primary.default \
     audio.primary.sm6150:32 \
     audio.r_submix.default \
@@ -113,8 +112,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/vendor/etc/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml \
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
